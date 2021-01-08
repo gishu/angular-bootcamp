9a341357b8b3b5831771f373358df4575dd4cbb5
Pillai, Gishu <gishu.r.pillai@bakerhughes.com>
2021-01-08 17:43:55 +0530

    3. Data binding

--- src/app/sample/sample.component.ts
+++ src/app/sample/sample.component.ts
@@ -3,13 +3,21 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'mx-sample',
   templateUrl: './sample.component.html',
-  styleUrls: [ './sample.component.css']
+  styleUrls: ['./sample.component.css']
 })
 export class SampleComponent implements OnInit {
+
+  myField = "2-way data binding";
+  buttonText = "Click me!";
+  enabled = false;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  toggleEnabledFlag() {
+    this.enabled = !this.enabled;
   }
 
 }
