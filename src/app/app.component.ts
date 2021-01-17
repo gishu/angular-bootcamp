6545f8f3bbb5df2979e81c4931dbda84d7ed7852
gishu <gishu@hotmail.com>
2021-01-18 01:17:45 +0530

    lifecycle methods to safely access @ViewChild

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { DisplayComponent } from './display/display.component';
 
 @Component({
@@ -6,16 +6,26 @@ import { DisplayComponent } from './display/display.component';
   templateUrl: "./app.component.html",
   styles: []
 })
-export class AppComponent {
+export class AppComponent implements OnInit, AfterViewInit {
   title = 'angular-bootcamp';
 
   x10 = 10;
 
   @ViewChild(DisplayComponent)
-  private childComponent : DisplayComponent;
+  private childComponent : DisplayComponent ;
 
   onCountChanged($event : { count : number}){
     console.log("AppComponent notified that Count is now " + $event.count);
     this.childComponent.setValue($event.count)
   }
+
+  ngOnInit(){
+    console.log("OnInit: Don't access ViewChild here - " + this.childComponent);
+  }
+
+  ngAfterViewInit(){
+    console.log("ngAfterViewInit: ViewChild should be set now");
+    console.log(this.childComponent);
+  }
+
 }
