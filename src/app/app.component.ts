90fc40901b06747fd000fe9ad11e53a9b375f049
Gishu Pillai <gishu@hotmail.com>
2021-07-02 16:01:18 +0530

    *ngIf to conditionally show content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ng-bootcamp';
+  enableText = true;
+  
+  toggleFlag(){
+    this.enableText = !this.enableText;
+  }
 }
