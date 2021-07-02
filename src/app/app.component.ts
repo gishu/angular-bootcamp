69065e255f3a94a5cce0e58437229091ba71c462
Gishu Pillai <gishu@hotmail.com>
2021-07-02 16:42:23 +0530

    ngStyle directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,21 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'ng-bootcamp';
   enableText = true;
-  
-  toggleFlag(){
+
+  toggleFlag() {
     this.enableText = !this.enableText;
+  }
+
+  getBGColor() {
+    return {
+      'font-style': 'italic',
+      'font-size.em': 2,
+      'background-color': (this.enableText? 'beige' : 'salmon'),
+    };
   }
 }
