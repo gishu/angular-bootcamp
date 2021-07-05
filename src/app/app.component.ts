e01e597ac00fe788606177aa76e929d11e831095
Gishu Pillai <gishu@hotmail.com>
2021-07-05 19:39:30 +0530

    Angular Component Lifecycle Hooks demo!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,21 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'ng-bootcamp';
+  appProp = '42';
+  items: number[] = [];
 
-  alertCustomContent(){
-    alert("Custom content has been projected!")
+  alertCustomContent() {
+    alert('Custom content has been projected!');
+    this.appProp = '84';
+  }
+  addItem() {
+    this.items.push(0);
+  }
+  removeItem(){
+    this.items.pop();
   }
 }
--- src/app/cli/cli.component.ts
+++ src/app/cli/cli.component.ts
@@ -1,4 +1,17 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import {
+  AfterContentChecked,
+  AfterContentInit,
+  AfterViewChecked,
+  AfterViewInit,
+  Component,
+  DoCheck,
+  Input,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+  SimpleChanges,
+  ViewEncapsulation,
+} from '@angular/core';
 
 @Component({
   // selector: 'mx-cli',
@@ -6,13 +19,53 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
   selector: '.mx-cli',
   templateUrl: './cli.component.html',
   styleUrls: ['./cli.component.css'],
-  encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None,
 })
-export class CliComponent implements OnInit {
+export class CliComponent
+  implements
+    OnInit,
+    OnChanges,
+    DoCheck,
+    AfterContentInit,
+    AfterContentChecked,
+    AfterViewInit,
+    AfterViewChecked,
+    OnDestroy
+{
+  @Input()
+  MyInput: string = 'undefined';
 
-  constructor() { }
+  constructor() {}
+  
 
-  ngOnInit(): void {
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('OnChanges - when bound property changes');
+    console.log(changes);
   }
 
+  ngOnInit(): void {
+    console.log('OnInit - component has been initialized');
+    console.log('Inputs ' + this.MyInput);
+  }
+
+  ngDoCheck(): void {
+    console.log('DoCheck - Angular change detection run!');
+  }
+
+  ngAfterContentInit(): void {
+    console.log('AfterContentInit() - ');
+  }
+
+  ngAfterContentChecked(): void {
+    console.log('AfterContentChecked() - ');
+  }
+  ngAfterViewInit(): void {
+    console.log('AfterViewInit() - ');
+  }
+  ngAfterViewChecked(): void {
+    console.log('AfterViewChecked() - ');
+  }
+  ngOnDestroy(): void {
+    console.log("OnDestroy() - ")
+  }
 }
