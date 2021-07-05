9bca4a07f9e6c5b6455eb6a4ecb0fa95bfd39ce7
Gishu Pillai <gishu@hotmail.com>
2021-07-05 20:54:04 +0530

    customize a directive with properties

--- src/app/better-highlight/better-highlight.directive.ts
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -3,6 +3,7 @@ import {
   ElementRef,
   HostBinding,
   HostListener,
+  Input,
   OnInit,
   Renderer2,
 } from '@angular/core';
@@ -13,13 +14,16 @@ import {
 export class BetterHighlightDirective implements OnInit {
   constructor(private elementRef: ElementRef) {}
 
+  @Input()
+  highlightColor : string = 'cyan';
+
   //DOM Properties are camel cased ; no hyphens
   @HostBinding('style.backgroundColor') private bgColor: string = 'transparent';
 
   ngOnInit() {}
 
   @HostListener('mouseenter') onMouseOver(eventData: Event) {
-    this.bgColor = 'cyan';
+    this.bgColor = this.highlightColor;
   }
   @HostListener('mouseleave') onMouseExit(eventData: Event) {
     this.bgColor = 'transparent';
